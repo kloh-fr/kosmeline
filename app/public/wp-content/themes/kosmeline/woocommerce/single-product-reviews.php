6346a180d9317ec48683d38943b898b159e7fe70
Luc Poupard <contact@kloh.ch>
2020-12-29 18:14:48 +0100

    Masquer le titre de l'onglet Avis

--- app/public/wp-content/themes/kosmeline/woocommerce/single-product-reviews.php
+++ app/public/wp-content/themes/kosmeline/woocommerce/single-product-reviews.php
@@ -26,7 +26,7 @@ if ( ! comments_open() ) {
 ?>
 <div id="reviews" class="woocommerce-Reviews">
 	<div id="comments">
-		<h2 class="woocommerce-Reviews-title">
+		<h2 class="woocommerce-Reviews-title screen-reader-text">
 			<?php
 			$count = $product->get_review_count();
 			if ( $count && wc_review_ratings_enabled() ) {
