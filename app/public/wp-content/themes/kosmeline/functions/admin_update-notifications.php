0f2fc26d96d1ce89a7a877d5e78af87fde1ad96d
Luc Poupard <contact@kloh.ch>
2023-01-14 10:49:40 +0100

    Fix create_function() dépréciée

--- app/public/wp-content/themes/kosmeline/functions/admin_update-notifications.php
+++ app/public/wp-content/themes/kosmeline/functions/admin_update-notifications.php
@@ -11,5 +11,10 @@
  */
 
 if ( ! current_user_can( 'update_plugins' ) ) {
-	add_action( 'admin_init', create_function( false, "remove_action( 'admin_notices', 'update_nag', 3 );" ) );
+	add_action( 'admin_init', 'remove_admin_notices' );
+
+	function remove_admin_notices() {
+		global $wp_filter;
+		remove_action( 'admin_notices', 'update_nag', 3 );
+	}
 }
